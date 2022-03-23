47ef541e8d891cf2314a8253108a0235359873bd
Nicolas Renard <nicolasrenard1999@gmail.com>
2022-03-23 08:59:00 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/'
+  ],
+  coverageProvider: 'v8',
+  // coverageReporters: [
+  //   "json",
+  //   "text",
+  //   "lcov",
+  //   "clover"
+  // ],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
