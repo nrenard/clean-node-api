31a944d104a36bd9c5dfdfc10f05f2773193a73f
Nicolas Renard <nicolasrenard1999@gmail.com>
2022-03-25 18:09:39 -0300

    feat: ensure cors is enable

---
+++ src/main/config/middlewares/cors.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const cors = (req: Request, res: Response, next: NextFunction): void => {
+  res.set('acess-control-allow-origin', '*')
+  res.set('acess-control-allow-methods', '*')
+  res.set('acess-control-allow-headers', '*')
+
+  next()
+}
--- src/main/config/middlewares/index.ts
+++ src/main/config/middlewares/index.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express'
 
 import { bodyParser } from './body-parser'
+import { cors } from './cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
