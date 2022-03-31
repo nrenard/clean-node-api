8c804ac871d3136497a79263b864e1aab1116e03
Nicolas Renard <nicolasrenard1999@gmail.com>
2022-03-31 08:53:57 -0300

    feat: ensure requests return json as default content type

--- src/main/config/middlewares/index.ts
+++ src/main/config/middlewares/index.ts
@@ -2,8 +2,10 @@ import { Express } from 'express'
 
 import { bodyParser } from './body-parser'
 import { cors } from './cors'
+import { contentType } from './content-type'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
